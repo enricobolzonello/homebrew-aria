dfb8ce85998b3ec0160f6d03acf7ec7439327789
Enrico Bolzonello <enribolzonello@gmail.com>
2025-10-29 23:58:30 +0100

    Added linux url and sha256

--- Formula/aria.rb
+++ Formula/aria.rb
@@ -1,9 +1,17 @@
 class Aria < Formula
   desc "The Aria Programming Language"
   homepage "https://egranata.github.io/aria/"
-  url "https://github.com/egranata/aria/releases/download/v0.9.20251013/aria-0.9.20251013-aarch64-apple-darwin-20251013222819.tgz"
-  sha256 "7c633b831114dd10454acbb601d5ef9b036c4a2e638302396bbed24cb75e6430"
   license "Apache-2.0"
+
+  if OS.mac? && Hardware::CPU.arm?
+      url "https://github.com/egranata/aria/releases/download/v0.9.20251013/aria-0.9.20251013-aarch64-apple-darwin-20251013222819.tgz"
+      sha256 "7c633b831114dd10454acbb601d5ef9b036c4a2e638302396bbed24cb75e6430"
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/egranata/aria/releases/download/v0.9.20251013/aria-0.9.20251013-x86_64-unknown-linux-gnu-20251013222823.tgz" 
+    sha256 "0ee1391aee2fb97affe51723c55c932ca30d8ef9bf4e26adc59f711a6387dd72"
+  else
+    odie "Aria is not supported on your system (unsupported OS or CPU architecture)"
+  end
 
   def install
     bin.install Dir["bin/*"] if Dir.exist?("bin")
